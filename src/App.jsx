aa719b5df982ad9eb01f981a6cb79686f3dafd17
Matheus Santos <somathsant@gmail.com>
2023-05-29 21:57:18 -0300

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Post} from "./Post"
+import { Header } from "./components/Header";
+
+import './styles.css'; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello ignite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header />
+      
+      <Post 
+        author="Matheus Santos"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quae totam, vitae, autem in rem excepturi cumque optio est perferendis nesciunt dolorum rerum doloribus aspernatur magnam, assumenda aliquam ex. Distinctio."
+      />
+    </div>
   )
 }
-
 export default App
---
+++ src/Post.jsx
@@ -0,0 +1,11 @@
+
+export function Post(props) {
+  console.log(props)
+  return (
+    <div>
+      <strong>{props.author}</strong>
+      <p>{props.content}</p>
+    </div>
+  )
+}
+
