5df6fd7289e9c640b7535cbf55349382a5ec0ea8
Matheus Santos <somathsant@gmail.com>
2023-05-31 10:14:48 -0300

    Adicao do componente de comentarios

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from './Comment.module.css'
+import {Trash, ThumbsUp} from 'phosphor-react'
+
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/diego3g.png"/>
+      
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24}/>
+            </button>
+          </header>
+          <p>Muito bom Matheus, parabéns!! </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+
+    </div>
+  )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 export function Post() {
 return (
@@ -31,6 +32,12 @@ return (
           <button type='submit'>Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
