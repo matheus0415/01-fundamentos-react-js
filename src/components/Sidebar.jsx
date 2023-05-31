2bc3225ce5e13b4979422f41da07c86830a2ad98
Matheus Santos <somathsant@gmail.com>
2023-05-30 22:06:20 -0300

    estilizacao da sidebar  e adicao de dependencia de icones phosphor

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
-import styles from './Sidebar.module.css';
+import {PencilLine} from 'phosphor-react'
 
+import styles from './Sidebar.module.css';
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
@@ -8,12 +9,14 @@ export function Sidebar() {
         src="https://plus.unsplash.com/premium_photo-1663047716627-e0b6c878761e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=40" 
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/matheus0415.png"/>
         <strong>Matheus Santos</strong>
         <span>Software Engineer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine />
           Editar seu perfil
         </a>
       </footer>
