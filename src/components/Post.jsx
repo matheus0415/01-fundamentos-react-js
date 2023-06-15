39b97bbfb76d25616b006ed7d232eb8acec35a1d
Matheus Santos <matheus.santos@fitbank.com.br>
2023-06-15 14:06:11 -0300

    ajuste de formato da data com data-fns

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,22 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
-export function Post({author, publishedAt} ) {
-return (
+
+export function Post({author, publishedAt, content} ) {
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  } );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
+  return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
@@ -13,13 +27,19 @@ return (
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:11:00">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Lorem ipsum dolor sit amet,</p>
-        <p> consectetur adipisicing elit. Obcaecati aspernatur quidem maxime!</p>
-        <p>Debitis suscipit ipsum expedita perferendis totam culpa quo laboriosam, deserunt facere ipsa quidem, molestiae, aut sed saepe dolores?</p>
+        {content.map(line => {
+          if (line.type == 'paragraph') {
+            return <p> {line.content}</p>
+          }else if (line.type == 'link'){
+            return <p> <a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
