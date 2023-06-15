2e7a50a34f206c60c2ed1848d5b2b1d2206ab02f
Matheus Santos <matheus.santos@fitbank.com.br>
2023-06-15 14:59:50 -0300

    Adicionando useState nos comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,19 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
+
+
 export function Post({author, publishedAt, content} ) {
+  
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -14,7 +22,15 @@ export function Post({author, publishedAt, content} ) {
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true,
-  })
+  });
+
+  function handleCreateNewComment() {
+
+    event.preventDefault();
+
+    setComments([...comments, comments.length + 1]);
+
+  };
 
   return (
     <article className={styles.post}>
@@ -42,7 +58,7 @@ export function Post({author, publishedAt, content} ) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea 
@@ -55,9 +71,9 @@ export function Post({author, publishedAt, content} ) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
